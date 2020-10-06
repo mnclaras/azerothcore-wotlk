28149a11272553aade3333ed44fc20086801509e
Mario Navarro Claras <mario94almunia@gmail.com>
2020-10-06 23:54:01 +0200

    Change ArenaPoints Received to 1/5 in 1v1

--- src/server/game/Battlegrounds/ArenaTeam.cpp
+++ src/server/game/Battlegrounds/ArenaTeam.cpp
@@ -623,6 +623,8 @@ uint32 ArenaTeam::GetPoints(uint32 memberRating)
         points *= 0.76f;
     else if (Type == ARENA_TEAM_3v3)
         points *= 0.88f;
+    else if (points)
+        points *= 0.2f;
 
     points *= sWorld->getRate(RATE_ARENA_POINTS);
 
