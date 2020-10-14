7bdada2a851ad1aec89f8e3a283e571d524c00c7
Mario Navarro Claras <mario94almunia@gmail.com>
2020-10-15 01:26:06 +0200

    Fix Hallows event #12

--- src/server/scripts/Custom/HolidayEvents/custom_hallows_end_event.cpp
+++ src/server/scripts/Custom/HolidayEvents/custom_hallows_end_event.cpp
@@ -446,8 +446,8 @@ public:
 
                 case EVENT_SPELL_RADIANCE:
                     //me->CastSpell((Unit*)NULL, SPELL_RADIANCE, false);
-					DoCastAOE(SPELL_RADIANCE);
-					// me->CastSpell(me, SPELL_RADIANCE, true);
+					//DoCastAOE(SPELL_RADIANCE);
+					me->CastSpell(me, SPELL_RADIANCE, true);
                     me->MonsterTextEmote(TEXT_RADIATE, 0, true);
                     _events.ScheduleEvent(EVENT_SPELL_RADIANCE, 6000, PHASE_FOUR);
                     break;
