0dc84075dc7169464d0bfebd5b2ef01c77011a46
Mario Navarro Claras <mario94almunia@gmail.com>
2021-03-28 13:41:13 +0200

    Separate reset stats in another npc

--- src/server/scripts/Custom/custom_npc_reset_cds.cpp
+++ src/server/scripts/Custom/custom_npc_reset_cds.cpp
@@ -38,39 +38,6 @@ public:
         return (locale == LOCALE_esES || locale == LOCALE_esMX);
     }
 
-    static bool HandleResetStatsOrLevelHelper(Player* player)
-    {
-        ChrClassesEntry const* classEntry = sChrClassesStore.LookupEntry(player->getClass());
-        if (!classEntry)
-        {
-            sLog->outError("Class %u not found in DBC (Wrong DBC files?)", player->getClass());
-            return false;
-        }
-
-        uint8 powerType = classEntry->powerType;
-
-        // reset m_form if no aura
-        if (!player->HasAuraType(SPELL_AURA_MOD_SHAPESHIFT))
-            player->SetShapeshiftForm(FORM_NONE);
-
-        player->setFactionForRace(player->getRace());
-
-        player->SetUInt32Value(UNIT_FIELD_BYTES_0, ((player->getRace()) | (player->getClass() << 8) | (player->getGender() << 16) | (powerType << 24)));
-
-        // reset only if player not in some form;
-        if (player->GetShapeshiftForm() == FORM_NONE)
-            player->InitDisplayIds();
-
-        player->SetByteValue(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_PVP);
-
-        player->SetUInt32Value(UNIT_FIELD_FLAGS, UNIT_FLAG_PVP_ATTACKABLE);
-
-        //-1 is default value
-        player->SetUInt32Value(PLAYER_FIELD_WATCHED_FACTION_INDEX, uint32(-1));
-        return true;
-    }
-
-
     bool OnGossipHello(Player* player, Creature* creature) override
     {
         bool isSpanish = IsSpanishPlayer(player);
@@ -116,16 +83,6 @@ public:
                     }
                 }
 
-
-                if (HandleResetStatsOrLevelHelper(player))
-                {
-                    player->InitRunes();
-                    player->InitStatsForLevel(true);
-                    player->InitTaxiNodesForLevel();
-                    player->InitGlyphsForLevel();
-                    player->InitTalentForLevel();
-                }
-
                 player->GetSession()->SendNotification(isSpanish ? "|cffFFFF00SERVICIOS \n |cffFFFFFFCDs reiniciados satisfactoriamente!" : "|cffFFFF00SERVICES \n |cffFFFFFFCooldowns succesfully reseted!");
                 player->CastSpell(player, 31726);
                 CloseGossipMenuFor(player);
@@ -136,18 +93,121 @@ public:
         return true;
     }
 
-	struct custom_npc_tools_reset_cdsAI : public ScriptedAI
-	{
+    struct custom_npc_tools_reset_cdsAI : public ScriptedAI
+    {
         custom_npc_tools_reset_cdsAI(Creature* creature) : ScriptedAI(creature) { }
-	};
+    };
 
-	CreatureAI* GetAI(Creature* creature) const override
-	{
-		return new custom_npc_tools_reset_cdsAI(creature);
-	}
+    CreatureAI* GetAI(Creature* creature) const override
+    {
+        return new custom_npc_tools_reset_cdsAI(creature);
+    }
+};
+
+class custom_npc_tools_reset_stats : public CreatureScript
+{
+public:
+    custom_npc_tools_reset_stats() : CreatureScript("custom_npc_tools_reset_stats") { }
+
+    static bool IsSpanishPlayer(Player* player)
+    {
+        LocaleConstant locale = player->GetSession()->GetSessionDbLocaleIndex();
+        return (locale == LOCALE_esES || locale == LOCALE_esMX);
+    }
+
+    static bool HandleResetStatsOrLevelHelper(Player* player)
+    {
+        ChrClassesEntry const* classEntry = sChrClassesStore.LookupEntry(player->getClass());
+        if (!classEntry)
+        {
+            sLog->outError("Class %u not found in DBC (Wrong DBC files?)", player->getClass());
+            return false;
+        }
+
+        uint8 powerType = classEntry->powerType;
+
+        // reset m_form if no aura
+        if (!player->HasAuraType(SPELL_AURA_MOD_SHAPESHIFT))
+            player->SetShapeshiftForm(FORM_NONE);
+
+        player->setFactionForRace(player->getRace());
+
+        player->SetUInt32Value(UNIT_FIELD_BYTES_0, ((player->getRace()) | (player->getClass() << 8) | (player->getGender() << 16) | (powerType << 24)));
+
+        // reset only if player not in some form;
+        if (player->GetShapeshiftForm() == FORM_NONE)
+            player->InitDisplayIds();
+
+        player->SetByteValue(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_PVP);
+
+        player->SetUInt32Value(UNIT_FIELD_FLAGS, UNIT_FLAG_PVP_ATTACKABLE);
+
+        //-1 is default value
+        player->SetUInt32Value(PLAYER_FIELD_WATCHED_FACTION_INDEX, uint32(-1));
+        return true;
+    }
+
+
+    bool OnGossipHello(Player* player, Creature* creature) override
+    {
+        bool isSpanish = IsSpanishPlayer(player);
+        AddGossipItemFor(player, GOSSIP_ICON_TRAINER, isSpanish ? "Reiniciar Stats." : "Reset Stats.", GOSSIP_SENDER_MAIN, 3,
+            isSpanish ? "Estas seguro de que quieres reiniciar Stats?" : "Are you sure you want to reset your Stats?", 0, false);
+        SendGossipMenuFor(player, DEFAULT_MESSAGE, creature->GetGUID());
+
+        return true;
+    }
+
+    bool OnGossipSelect(Player* player, Creature* /*creature*/, uint32 sender, uint32 action) override
+    {
+        player->PlayerTalkClass->ClearMenus();
+
+        if (sender == GOSSIP_SENDER_MAIN)
+        {
+            bool isSpanish = IsSpanishPlayer(player);
+
+            switch (action)
+            {
+            case 3: // Reset Cooldowns
+                if (player->IsInCombat())
+                {
+                    player->GetSession()->SendNotification(isSpanish ? "Error! Estas en combate." : "Failure! You are in combat.");
+                    CloseGossipMenuFor(player);
+                    return false;
+                }
+
+                if (HandleResetStatsOrLevelHelper(player))
+                {
+                    player->InitRunes();
+                    player->InitStatsForLevel(true);
+                    player->InitTaxiNodesForLevel();
+                    player->InitGlyphsForLevel();
+                    player->InitTalentForLevel();
+                }
+
+                player->GetSession()->SendNotification(isSpanish ? "|cffFFFF00SERVICIOS \n |cffFFFFFFStats reiniciados satisfactoriamente!" : "|cffFFFF00SERVICES \n |cffFFFFFFStats succesfully reseted!");
+                player->CastSpell(player, 31726);
+                CloseGossipMenuFor(player);
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    struct custom_npc_tools_reset_statsAI : public ScriptedAI
+    {
+        custom_npc_tools_reset_statsAI(Creature* creature) : ScriptedAI(creature) { }
+    };
+
+    CreatureAI* GetAI(Creature* creature) const override
+    {
+        return new custom_npc_tools_reset_statsAI(creature);
+    }
 };
 
 void AddSC_custom_npc_tools_reset_cds()
 {
-	new custom_npc_tools_reset_cds();
+    new custom_npc_tools_reset_cds();
+    new custom_npc_tools_reset_stats();
 }
