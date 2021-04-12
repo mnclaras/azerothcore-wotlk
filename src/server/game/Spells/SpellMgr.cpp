62dd21492bc36b0882ad47ef5164d9258efc9a25
Mario Navarro Claras <mario94almunia@gmail.com>
2021-04-12 02:20:12 +0200

    Try to fix pet dash #7

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3709,9 +3709,11 @@ void SpellMgr::LoadDbcDataCorrections()
         /////////////////////////////////
         // Dash
         case 61684:
-            spellInfo->EffectImplicitTargetA[1] = TARGET_UNIT_PET;
-            //spellInfo->EffectImplicitTargetB[2] = spellInfo->EffectImplicitTargetB[1] = spellInfo->EffectImplicitTargetB[0] = 0;
-            //spellInfo->EffectImplicitTargetA[2] = spellInfo->EffectImplicitTargetA[1] = spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER;
+            //spellInfo->EffectImplicitTargetA[1] = TARGET_UNIT_PET;
+            //spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER;
+            //spellInfo->EffectImplicitTargetB[0] = TARGET_UNIT_CASTER;
+            spellInfo->EffectImplicitTargetB[2] = spellInfo->EffectImplicitTargetB[1] = spellInfo->EffectImplicitTargetB[0] = 0;
+            spellInfo->EffectImplicitTargetA[2] = spellInfo->EffectImplicitTargetA[1] = spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER;
 
             break;
             //spellInfo->EffectImplicitTargetA[1] = TARGET_UNIT_PET;
