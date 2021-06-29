45e881311a1f5a8bee45ee9ba0d842387ab5d4c7
mnclaras <mario.navarro@gmail.com>
2021-06-29 21:13:13 +0000

    Warden fix warnings

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -70,9 +70,9 @@ _flags(AFLAG_NONE), _effectsToApply(effMask), _needClientUpdate(false), _disable
 #endif
         }
         else {
-#if defined(ENABLE_EXTRAS) && defined(ENABLE_EXTRA_LOGS)
-            sLog->outError(LOG_FILTER_SPELLS_AURAS, "Aura: %u Effect: %d could not find empty unit visible slot", GetBase()->GetId(), GetEffectMask());
-#endif
+// #if defined(ENABLE_EXTRAS) && defined(ENABLE_EXTRA_LOGS)
+//             sLog->outError(LOG_FILTER_SPELLS_AURAS, "Aura: %u Effect: %d could not find empty unit visible slot", GetBase()->GetId(), GetEffectMask());
+// #endif
         }
     }
     _InitFlags(caster, effMask);
